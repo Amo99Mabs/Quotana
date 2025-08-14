1dbb862f376d81afb01d18a1cb215530f4255e60
Amo99Mabs <174100269+Amo99Mabs@users.noreply.github.com>
2025-08-14 20:19:30 +0200

    Add random inspirational quote generator using Python's random module
    This script selects and prints a random motivational quote from a predefined list.
    Includes quotes from Steve Jobs, Eleanor Roosevelt, Franklin D. Roosevelt, and Abraham Lincoln.
    Useful for CLI-based inspiration or as a module in larger projects."

---
+++ quote.py
@@ -0,0 +1,13 @@
+import random
+
+quotes = [
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "Innovation distinguishes between a leader and follower. - Steve Jobs ",
+    "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+    "The only limit to our realizationof tommorrow will be our doubts of today, - Franklin D. Roosevelt",
+    "The best wayto predict the future is to create it. - Abraham Lincoln"
+]
+
+random_quote = random.choice(quotes)
+print("Random Quote:")
+print(random_quote)
